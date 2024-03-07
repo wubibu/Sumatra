632122aff489b7967353bfe35c5e290bfe290e7d
wubibu <162594730+wubibu@users.noreply.github.com>
2024-03-07 22:09:09 +0700

    Delete test directory

--- test/test_display_names.py
+++
@@ -1,6 +0,0 @@
-# test_display_names.py
-
-from display_names import display_names
-
-def test_display_names():
-    assert display_names() == ["Member 1", "Member 2", "Member 3"]  # Sesuaikan dengan hasil yang diharapkan
