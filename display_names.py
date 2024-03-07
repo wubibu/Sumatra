83360a6348f151affabfe8088f0210d9a6c732eb
wubibu <162594730+wubibu@users.noreply.github.com>
2024-03-07 21:17:12 +0700

    Create display_names.py

---
+++ display_names.py
@@ -0,0 +1,9 @@
+# display_names.py
+
+def main():
+    names = ["Member 1", "Member 2", "Member 3"]  # Ganti dengan nama anggota kelompok Anda
+    for name in names:
+        print(name)
+
+if __name__ == "__main__":
+    main()
